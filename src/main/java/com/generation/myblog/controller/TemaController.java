7ccef1a47149e66529230b1187c9ab9711f5afb2
carolinebraz <jcarolinebraz@gmail.com>
2023-06-27 19:59:49 -0300

    Adiciona método de apagar tema

--- src/main/java/com/generation/myblog/controller/TemaController.java
+++ src/main/java/com/generation/myblog/controller/TemaController.java
@@ -1,18 +1,22 @@
 package com.generation.myblog.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.myblog.model.Tema;
 import com.generation.myblog.repository.TemaRepository;
@@ -53,6 +57,18 @@ public class TemaController {
 		return temaRepository.findById(tema.getId())
 				.map(resp -> ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema)))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Tema> tema = temaRepository.findById(id);
+
+		if (tema.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+		temaRepository.deleteById(id);
+		
 	}
 
 }
