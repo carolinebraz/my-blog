463974aff1b0d58fe2f55e23f3a37d434464a996
carolinebraz <jcarolinebraz@gmail.com>
2023-06-27 19:31:23 -0300

    Adiciona a classe Tema na camada model

---
+++ src/main/java/com/generation/myblog/model/Tema.java
@@ -0,0 +1,37 @@
+package com.generation.myblog.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
