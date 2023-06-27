9ce2167c588c841d3b2316a3ae500f1ab19bd198
carolinebraz <jcarolinebraz@gmail.com>
2023-06-27 19:47:31 -0300

    Adiciona a classe TemaController na camada controller e o método para listar todos os temas

---
+++ src/main/java/com/generation/myblog/controller/TemaController.java
@@ -0,0 +1,28 @@
+package com.generation.myblog.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.myblog.model.Tema;
+import com.generation.myblog.repository.TemaRepository;
+
+@RestController
+@RequestMapping("/temas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TemaController {
+
+	@Autowired
+	private TemaRepository temaRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Tema>> getAll() {
+		return ResponseEntity.ok(temaRepository.findAll());
+	}
+
+}
